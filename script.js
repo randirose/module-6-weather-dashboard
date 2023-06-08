8ac955679dad91075432cfc13141b4d02cc88482
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-06-08 14:00:29 -0700

    working on api fetch calls for coordinates

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function (){
 var today = dayjs().format('dddd, MMMM DD, YYYY');
 var city = $('#city-input').val();
+
+$('#submit').on('click', function(e) {
+    e.preventDefault();
+    getCoords();
+})
+
+function getCoords() {
+    var url="http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=36ded1ba363e28fda838ee1a00dc51af";
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
 function getWeather() {
-    var url = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=36ded1ba363e28fda838ee1a00dc51af';
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=36ded1ba363e28fda838ee1a00dc51af";
     fetch(url)
     .then(function (response) {
-        return response.jason();
+        return response.json();
     })
 }
 
