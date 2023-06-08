8df56eed1e2ed91d2b55192522b45a0fde9d6aa8
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-06-08 15:04:31 -0700

    added displays for weather data, icon, etc.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-$(document).ready(function (){
+// $(document).ready(function (){
 var today = dayjs().format('dddd, MMMM DD, YYYY');
-var city = $('#city-input').val();
 
-$('#submit').on('click', function(e) {
-    e.preventDefault();
-    getCoords();
-})
+var apiKey = "36ded1ba363e28fda838ee1a00dc51af";
+
+
 
 function getCoords() {
+    var city = $('#city-input').val();
     var url="http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=36ded1ba363e28fda838ee1a00dc51af";
     fetch(url)
     .then(function(response) {
@@ -15,29 +14,52 @@ function getCoords() {
     })
     .then(function (data) {
         console.log(data);
-    })
-}
+        getWeather();
+    
+
+
+
 
 function getWeather() {
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=36ded1ba363e28fda838ee1a00dc51af";
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=36ded1ba363e28fda838ee1a00dc51af";
     fetch(url)
     .then(function (response) {
         return response.json();
     })
+    .then(function (data) {
+        console.log(data);
+        var icon = document.createElement('img');
+        icon.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        
+        $("#city-name").text(data.name + " | " + today);
+        var iconPlacement = document.getElementById("city-name");
+        iconPlacement.appendChild(icon);
+        $("#description").text(data.weather[0].description);
+        $("#temp").text("Tempurature: " + data.main.temp + "°F");
+        $("#feels-like").text("Feels like: " + data.main.feels_like + "°F");
+        $("#wind").text("Wind speed: " + data.wind.speed + "MPH");
+        $("#humidity").text("Humidity: " + data.main.humidity + "%");
+    })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+}
+$('#submit').on('click', function(e) {
+    e.preventDefault();
+    getCoords();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// })
